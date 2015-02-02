994557eac20cb587f474dfdc4d7b24a0f7453e24
LeonM234 <LeonM234@yahoo.com>
2015-02-01 19:23:33 -0800

    Proper implementation of "isItPrime"

--- Mathgician/Program.cs
+++ Mathgician/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,14 +17,74 @@ namespace Mathgician
             Console.WriteLine("Ok.  I'm gonna do some " + desiredMaths);
             if ("Integers" == desiredMaths)
             {
-                // Do something here
+                PrintIntegers();
             }
-            else 
+            else if ("Fibonacci" == desiredMaths)
+            {
+                PrintFibonacciSequence();
+            }
+            else if ("Primes" == desiredMaths)
+            {
+                PrintPrimes();
+            }
+            else
             {
                 Console.WriteLine("Well, Actually... I Lied...");
             }
             Console.WriteLine("Press Any Key to Exit...");
             Console.ReadKey();
+        }
+
+        private static void PrintPrimes()
+        {
+            List<int> primes = new List<int>();
+            primes.Add(2); 
+            int counter = 3;
+            Console.WriteLine(2);
+            while (true)
+            {
+                bool isItPrime = true;
+                for (int i = 0; i < primes.Count; i++)
+                { 
+                    if (counter % primes[i] == 0)
+                    {
+                        isItPrime = false;
+                        break;
+                    } 
+                }
+                if (isItPrime == true)
+                {
+                    Console.WriteLine(counter);
+                    primes.Add(counter);
+                        
+                }
+                System.Threading.Thread.Sleep(100);
+                counter += 2;
+            }
+        }
+
+        private static void PrintFibonacciSequence()
+        {
+            int previous = 0;
+            int current = 1;
+            while (true)
+            {
+                Console.WriteLine(current);
+                System.Threading.Thread.Sleep(100);
+                int newValue = previous + current;
+                previous = current;
+                current = newValue;
+            }
+        }
+
+        private static void PrintIntegers()
+        {
+            int nextInteger = 0;
+            while (true)
+            {
+                nextInteger++;
+                Console.WriteLine(nextInteger);
+            }
         }
     }
 }
